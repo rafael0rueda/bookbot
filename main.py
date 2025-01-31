31e0dc51d409cbf6436bdc12c6624ac7acdb85d8
rafael0rueda <rrrm85@gmail.com>
2025-01-31 09:55:23 -0500

    add report

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contens = f.read()
         cw = count_words(file_contens)
-        print(count_characters(file_contens))
+        cc = count_characters(file_contens)
+        print(f"--- Begin report of {f.name} ---")
+        print(f"{cw} words found in the document\n")
+        report(cc)
+        
 
 def count_words(text):
     words = text.split()
@@ -18,5 +22,16 @@ def count_characters(text):
         else:
             count_ch[ch] = 1
     return count_ch
+
+def sort_on(dict):
+    return dict["num"]
+
+def report(count_ch):
+    for ch in count_ch:
+        if ch.isalpha():
+            print(f"The '{ch}' character was found {count_ch[ch]} times")
+    
+    return
+
 
 main()
